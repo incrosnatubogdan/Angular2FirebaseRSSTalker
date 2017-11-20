72bfe2f26fd97d9c073dda21de141d4120d6a77d
takestar15 <tkim3182@gmail.com>
2017-11-20 13:47:30 -0500

    Implement speech recognition

--- src/app/items/courses-list/courses-list.component.ts
+++ src/app/items/courses-list/courses-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Component, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { AngularFireDatabase, AngularFireAction } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
@@ -13,13 +13,15 @@ import { SpeechService } from '../../shared/speech/speech.service';
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.css']
 })
-export class CoursesListComponent {
+export class CoursesListComponent implements OnDestroy {
   @ViewChildren('courseList') courseList: QueryList<any>;
 
   items$: Observable<AngularFireAction<firebase.database.DataSnapshot>[]>;
   size$: BehaviorSubject<string|null>;
+  waitCommand: boolean;
 
   constructor(db: AngularFireDatabase, private speech: SpeechService) {
+    this.waitCommand = false;
     this.size$ = new BehaviorSubject(null);
     this.items$ = this.size$.switchMap(category =>
       db.list('/courses', ref =>
@@ -32,14 +34,47 @@ export class CoursesListComponent {
     this.size$.next(category);
   }
 
-  speak(text: string) {
-    this.speech.speak(text);
+  startSpeaking() {
+    this.waitCommand = false;
+    this.courseList.forEach(elem => elem.nativeElement.style.border = null);
+
+    Observable.of(...this.courseList.toArray())
+      .concatMap(text => {
+        text.nativeElement.style.border = '1px solid red';
+        return this.speech.speak(text.nativeElement.textContent);
+      })
+      .subscribe(
+        (next) => console.log('next'),
+        (error) => console.log(error),
+        () => {
+          this.startRecording();
+        }
+      );
   }
 
-  startSpeaking() {
-    this.courseList.forEach(courseElem => {
-      this.speak(courseElem.nativeElement.textContent);
-    })
+  startRecording() {
+    this.waitCommand = true;
+
+    this.speech.record().subscribe(
+      event => {
+        const command = event.results[0][0].transcript;
+        console.log(command);
+        if (command === 'read more') {
+          this.startSpeaking();
+        } else {
+          this.waitCommand = false;
+        }
+      },
+      error => {
+        console.log(error);
+        if (error === 'no-speech') {
+          this.startRecording();
+        }
+      },
+      () => {
+        this.waitCommand = false;
+      }
+    )
   }
 }
 
--- src/app/shared/speech/speech.service.ts
+++ src/app/shared/speech/speech.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 interface IWindow extends Window {
   webkitSpeechRecognition: any;
@@ -10,13 +11,39 @@ interface IWindow extends Window {
 @Injectable()
 export class SpeechService {
   speechSynthesis: any;
+  speechRecognition: any;
 
   constructor() { }
 
-  speak(text: string) {
-    const { speechSynthesis, SpeechSynthesisUtterance }: IWindow = <IWindow>window;
-    this.speechSynthesis = speechSynthesis;
-    const utterText = new SpeechSynthesisUtterance(text);
-    this.speechSynthesis.speak(utterText);
+  speak(text: string): Observable<any> {
+    return Observable.create(observer => {
+      const { speechSynthesis, SpeechSynthesisUtterance }: IWindow = <IWindow>window;
+      this.speechSynthesis = speechSynthesis;
+      const utterText = new SpeechSynthesisUtterance(text);
+      utterText.onend = e => {
+        observer.next(e);
+        observer.complete();
+      };
+
+      this.speechSynthesis.speak(utterText);
+    });
+  }
+
+  record(): Observable<string> {
+    return Observable.create(observer => {
+      const { webkitSpeechRecognition }: IWindow = <IWindow>window;
+      const speechRecognition = new webkitSpeechRecognition();
+
+      speechRecognition.onresult = event => {
+        observer.next(event);
+        observer.complete();
+      };
+
+      speechRecognition.start();
+
+      return () => {
+        speechRecognition.stop();
+      }
+    });
   }
 }
